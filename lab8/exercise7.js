ad54e00b2a672f8a843d5980b29fadaab48813b6
Agnes <126595515+agnieszkakraczkowska@users.noreply.github.com>
2024-05-15 12:24:32 +0200

    Create exercise7.js

---
+++ lab8/exercise7.js
@@ -0,0 +1,36 @@
+const arr = [
+    [ 66, 97, 114, 100, 4, 2, 110, 11, 1, 6, 20 ],
+    [ 99, 3, 10, 122, 76, 101, 111, 3, 32, 100, 0 ],
+    [ 6, 22, 1, 111, 32, 10, 110, 7, 97, 97, 67 ],
+    [ 60, 97, 116, 32, 100, 23, 97, 114, 100, 32, 34 ],
+    [ 2, 106, 15, 6, 111, 56, 80, 20, 10, 86, 10 ],
+    [ 20, 110, 121, 32, 107, 55, 50, 99, 110, 105, 8 ],
+    [ 12, 9, 22, 102, 66, 100, 12, 105, 50, 76, 110 ],
+    [ 42, 81, 123, 92, 26, 98, 20, 1, 20, 11, 10 ],
+];
+
+const str = "rrrdddllddrrruuuurrddrruurddddlld";
+
+let row = 0;
+let column = 0;
+let secretInformation = "";
+secretInformation += String.fromCharCode(arr[row][column]);
+for(let character of str) { 
+    switch(character) { 
+        case 'r':
+            column++;
+            break;
+        case 'l':
+            column--;
+            break;
+        case 'u':
+            row--;
+            break;
+        case 'd':
+            row++;
+            break;
+    }
+    secretInformation += String.fromCharCode(arr[row][column]);
+}
+
+console.log(secretInformation);
